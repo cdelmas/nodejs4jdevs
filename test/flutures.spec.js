603879ec763371b643f7e640ee2d368b37c28e61
cdelmas <cyril.delmas@gmail.com>
2018-03-26 07:01:21 +0200

    test: change fluture test example

--- test/flutures.spec.js
+++ test/flutures.spec.js
@@ -1,14 +1,11 @@
-
 const Future = require('fluture');
 const request = require('request-promise-native');
 
-const compute = (a, c) => {
-  const b = Math.floor(a + (c * 100));
-  return Future.encaseP(request)(`https://www.qwant.com/?q=fluture%20js%20${b}&t=all`);
-};
+const search = (term, r) =>
+  Future.encaseP(request)(`https://www.qwant.com/?q=${term}%20${r}&t=all`);
 
 test('future as pure data, no side effect, no mocks', () => {
-  const future = compute(35, Math.random());
+  const future = search('fluture js', Math.random());
   expect(future).toBeInstanceOf(Future);
   expect(future._a).toMatch(/.*qwant\.com.*/); // eslint-disable-line no-underscore-dangle
   expect(future._fn).toHaveProperty('get'); // eslint-disable-line no-underscore-dangle
