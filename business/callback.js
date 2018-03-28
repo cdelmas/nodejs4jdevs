46b0e42ada9a9ca80cbbc0a9b95852264881c64c
cdelmas <cyril.delmas@gmail.com>
2018-03-28 06:28:39 +0200

    feat: final example

--- business/callback.js
+++ business/callback.js
@@ -1,7 +1,5 @@
 const request = require('request');
 
-// TODO: search two things and combine the result
-
 const search = (term, r, cb) => {
   request({
     url: `https://www.qwant.com/?q=${term}%20${r}&t=all`,
@@ -11,4 +9,25 @@ const search = (term, r, cb) => {
   });
 };
 
-module.exports = { search };
+const combine = (a, b) => ({
+  ...a,
+  ...b,
+});
+
+const business = (r, cb) => {
+  search('callback', r, (err, data) => {
+    if (!err) {
+      search('hell', r, (err2, data2) => {
+        if (err2) {
+          cb(err2);
+        } else {
+          cb(null, combine(data, data2));
+        }
+      });
+    } else {
+      cb(err);
+    }
+  });
+};
+
+module.exports = { business };
--- test/callback.spec.js
+++ test/callback.spec.js
@@ -1,26 +1,30 @@
 const request = require('request');
-const { search } = require('../business/callback.js');
+const { business } = require('../business/callback.js');
 
 jest.mock('request');
 
 test('should give an error', (done) => {
   request.mockImplementationOnce((url, cb) => cb(new Error('some http error')));
-  search('callback hell', 42, (err, res, body) => {
+  business(13, (err, body) => {
     expect(err).toBeDefined();
-    expect(res).toBeUndefined();
     expect(body).toBeUndefined();
     done();
   });
 });
 
+// should test the second error path...
+
 // do not forget the case where statusCode !== 2xx
 
 test('should do the stuff', (done) => {
-  request.mockImplementationOnce((url, cb) => cb(null, { statusCode: 200 }, { data: 42 }));
-  search('callback hell', 42, (err, body) => {
+  request
+    .mockImplementationOnce((url, cb) => cb(null, { statusCode: 200 }, { data: 42 }))
+    .mockImplementationOnce((url, cb) => cb(null, { statusCode: 200 }, { data2: 666 }));
+  business(33, (err, body) => {
     expect(err).toBeNull();
     expect(body).toMatchObject({
       data: 42,
+      data2: 666,
     });
     done();
   });
